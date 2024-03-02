ef8e91963885a2c9a9dea5dc7f494a50c808a511
Fida op <fidzandroid@gmail.com>
2024-03-02 15:22:17 +0530

    coupon

--- helpers/cartHelper.js
+++ helpers/cartHelper.js
@@ -132,15 +132,17 @@ async function getCart(userId) {
 
 
 
-  function calculateSubtotal(cart) {
+  async function calculateSubtotal(cart) {
     console.log('caerfffffff',cart)
     let subtotal = 0;
     for (const item of cart.items) {
       // Ensure both quantity and price are present and are numbers
       if (typeof item.quantity === 'number' && typeof item.product.price === 'number') {
         if (item.product.coupon) {
+          const coupon = await Coupon.findById(item.product.coupon).lean().exec();
             const discountedPrice = item.product.price - (item.product.price * item.product.coupon.discountPercentage / 100);
             subtotal += item.quantity * discountedPrice;
+           
         } else {
             subtotal += item.quantity * item.product.price;
         }
